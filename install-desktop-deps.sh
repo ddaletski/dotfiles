e2e298e43412e7b5d969301014d59aef02313116
Denis Daletski <denis@ux430unr>
2018-04-05 19:34:01 +0300

    deps

---
+++ install-desktop-deps.sh
@@ -0,0 +1,3 @@
+sudo apt install tilda rofi volumeicon-alsa compton xfce4-power-manager blueman xxkb conky xf ce4-terminal lightdm nautilus network-manager lxappearance feh nitrogen python3-pip imagemagick scrot fonts-font-awesome network-manager-gnome
+
+sudo pip3 install py3status
