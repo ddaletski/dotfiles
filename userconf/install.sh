39fff6e0ce0a5ede5bcda85072caa6217e642e81
Dzianis Daletski <ddaletski@ddaletski-0Y2MD6R.dyn.pinadmin.com>
2022-02-07 15:01:20 +0100

    systemctl: os check

--- userconf/install.sh
+++ userconf/install.sh
@@ -109,9 +109,9 @@ fi
 
 
 # systemd user configs
-mkdir -p $HOME/.config/systemd/user
-
-export NGROK_EXEC=$(which ngrok)
-cat templates/ngrok-ssh.service | envsubst > $HOME/.config/systemd/user/ngrok-ssh.service
-systemctl --user daemon-reload
-systemctl --user enable --now ngrok-ssh.service
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    export NGROK_EXEC=$(which ngrok)
+    cat templates/ngrok-ssh.service | envsubst > $HOME/.config/systemd/user/ngrok-ssh.service
+    systemctl --user daemon-reload
+    systemctl --user enable --now ngrok-ssh.service
+fi
